8688f84d19c1b7189f76d741a13afc5a601ec6db
Daniil-Solo <daniil.solo1723@gmail.com>
2024-11-14 03:16:01 +0500

    update gitignore

--- backend/src/obsirag_backend/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-from obsirag_backend.api.messages.routes import router as messages_router
-
-app = FastAPI(title="Obsidian RAG Backend")
-
-app.include_router(messages_router)
