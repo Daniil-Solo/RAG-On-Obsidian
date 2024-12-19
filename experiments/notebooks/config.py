ab4ab49434ff0b2a65e026d050bfd7964091f6b1
Abdullin Ilgiz <abdyllin-ilgiz@mail.ru>
2024-12-20 00:10:38 +0300

    Finished retrieve testing

--- experiments/notebooks/config.py
+++
@@ -1,5 +0,0 @@
-from qdrant_client.http.models import Distance
-
-vector_similarty_metric = Distance.COSINE
-# embedder_name = 'cointegrated/rubert-tiny2' # 0.3771428571428571
-embedder_name = 'intfloat/multilingual-e5-large-instruct'  # 0.8766666666666667
