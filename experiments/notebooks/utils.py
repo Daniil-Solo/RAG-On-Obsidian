17744ef60d9df911a1889717fa074ff0b0788fa5
Ilgiz Abdullin <91130064+abdullinilgiz@users.noreply.github.com>
2024-12-21 22:39:09 +0300

    Retrieve (#13)
    Retrieve baseline and evaluation

--- experiments/notebooks/utils.py
+++ experiments/notebooks/utils.py
@@ -3,30 +3,32 @@ from embedder import get_sentences_embeddings
 
 
 def file_into_db_collection(
-        root, filename, db_client, text_splitter, sentence_embedder):
+        root, filename, db_client, text_splitter, sentence_embedder, prefix,
+        collection_name="obsidian-vault"):
     if filename.endswith(".md"):
         file_path = os.path.join(root, filename)
         with open(file_path, 'r') as file:
             content = file.read()
             splitted_texts = text_splitter.split_text(content)
-            embeds = get_sentences_embeddings(
-                splitted_texts, sentence_embedder)
-            payload = [{
-                'file_path': file_path,
-                'filename': filename,
-                'text': text, }
-                for text in splitted_texts]
-            db_client.upload_collection(
-                collection_name="obsidian-vault",
-                vectors=embeds,
-                payload=payload
-            )
-            return splitted_texts
+            for text_chunk in splitted_texts:
+                embeds = get_sentences_embeddings(
+                    [text_chunk], sentence_embedder, prefix)
+                payload = [{
+                    'file_path': file_path,
+                    'filename': filename,
+                    'text': text_chunk}]
+                db_client.upload_collection(
+                    collection_name=collection_name,
+                    vectors=embeds,
+                    payload=payload
+                )
 
 
 def folder_into_db_collection(
-        folder_path, db_client, text_spliltter, sentence_embedder):
+        folder_path, db_client, text_spliltter, sentence_embedder, prefix,
+        collection_name="obsidian-vault"):
     for root, _, files in os.walk(folder_path):
         for filename in files:
             file_into_db_collection(
-                root, filename, db_client, text_spliltter, sentence_embedder)
+                root, filename, db_client, text_spliltter, sentence_embedder,
+                prefix, collection_name)
